ff75a277dadfb8a0d5894b56b57aa76635b5d451
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2025-07-30 22:48:29 +0200

    🎨 dark mode support

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google'
 
 import './globals.css'
 import { OpenAiKeyProvider } from '../components/OpenAiKeyContext'
+import { ThemeProvider } from '@/components/ThemeProvider'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -25,11 +26,18 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <OpenAiKeyProvider>{children}</OpenAiKeyProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <OpenAiKeyProvider>{children}</OpenAiKeyProvider>
+        </ThemeProvider>
       </body>
     </html>
   )
